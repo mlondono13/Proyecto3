3df7a6c23835593b8a381ad0fcae6486ddacd209
mlondono13 <mlondonol@eafit.edu.co>
2025-06-01 14:04:59 -0500

    Updated script

--- api_to_s3.py
+++ api_to_s3.py
@@ -1,13 +1,29 @@
 import requests
+import subprocess
 import os
-from datetime import datetime
-# Descargar y guardar los datos de la API
+
+BUCKET = "proyecto3-bigdata"
+BASE_PATH = "raw/api"
+
+# Crear archivos y subirlos
 endpoints = ["products", "users", "carts"]
 for endpoint in endpoints:
     url = f"https://fakestoreapi.com/{endpoint}"
     response = requests.get(url)
+
     if response.status_code == 200:
-        date = datetime.now().strftime("%Y%m%d_%H%M%S")
-        filename = f"{endpoint}_{date}.json"
+        filename = f"{endpoint}.json"
+        
+        # Guardar archivo localmente
+        with open(filename, "w") as f:
+            f.write(response.text)
+        
+        # Subir a S3 usando AWS CLI
+        s3_key = f"s3://{BUCKET}/{BASE_PATH}/{endpoint}/{filename}"
+        subprocess.run(["aws", "s3", "cp", filename, s3_key], check=True)
+        print(f"Subido a: {s3_key}")
+        
+        # (Opcional) eliminar archivo local
+        os.remove(filename)
     else:
-        print(f"[✗] Error al descargar {endpoint}")
+        print(f"Error al obtener datos de {endpoint}")
