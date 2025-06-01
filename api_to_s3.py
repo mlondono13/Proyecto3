fa7e075e7bc5fd43966c5c2f5ebb14dc65d693d8
mlondono13 <mlondonol@eafit.edu.co>
2025-06-01 14:02:18 -0500

    api to bucket

--- api_to_s3.py
+++ api_to_s3.py
@@ -1,11 +1,6 @@
 import requests
 import os
 from datetime import datetime
-
-# Carpeta local donde se guardarán los archivos
-OUTPUT_DIR = "raw_data"
-os.makedirs(OUTPUT_DIR, exist_ok=True)
-
 # Descargar y guardar los datos de la API
 endpoints = ["products", "users", "carts"]
 for endpoint in endpoints:
@@ -14,9 +9,5 @@ for endpoint in endpoints:
     if response.status_code == 200:
         date = datetime.now().strftime("%Y%m%d_%H%M%S")
         filename = f"{endpoint}_{date}.json"
-        filepath = os.path.join(OUTPUT_DIR, filename)
-        with open(filepath, "w") as f:
-            f.write(response.text)
-        print(f"[✓] {endpoint} guardado como {filepath}")
     else:
         print(f"[✗] Error al descargar {endpoint}")
